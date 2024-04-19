032ed2eef8672d17f5b081780077e63e9716602b
maxzuric08 <162721708+maxzuric08@users.noreply.github.com>
2024-04-19 11:28:22 -0400

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,42 @@
 # Replace the "ANSWER HERE" for your answer
 
 def roots(a, b, c):
-    return "ANSWER HERE"
+    x1=(-b+((b**2)-(4*a*c))**(1/2))/(2*a)
+    x2=(-b-((b**2)-(4*a*c))**(1/2))/(2*a)
+    if x1==x2:
+        return "("+str(x1)+")"
+    elif x1!=x2 and 'j' in str(x1) and 'j' in str(x2):
+        return '( )'
+    elif x1!=x2:
+        return "("+str(x1)+", "+str(x2)+")"
+    
 
 
 def value_y(a, b, c, x):
-    return "ANSWER HERE"
+    y= a*(x**2)+b*x+c
+    return y
 
 
 def to_string(a, b, c):
-    return "ANSWER HERE"
+    if a!=0 and b!=0 and c!=0:
+        return 'f(x) = '+str(a)+' * X^2 + '+str(b)+' * X + '+str(c)
+    elif a==0 and b!=0 and c!=0:
+        return 'f(x) = '+str(b)+' * X + '+str(c)
+    elif a!=0 and b==0 and c!=0:
+        return 'f(x) = '+str(a)+' * X^2 + '+str(c)
+    elif a==0 and b==0 and c!=0:
+        return 'f(x) = '+str(c)
 
 
-def derivation(a, b):
-    return "ANSWER HERE"
+
+def derivation(a, b, c):
+    deriv1=str(a*2)+' * X + '+str(b)
+    if a!=0 and b!=0:
+        return "f'(x) = "+str(deriv1)
+    elif a==0 and b!=0:
+        return "f'(x) = "+str(b)
+    elif a!=0 and b==0:
+        return "f'(x) = "+str(a*2)+' * X'
+    elif a==0 and b==0:
+        return "f'(x) = 0"
+
